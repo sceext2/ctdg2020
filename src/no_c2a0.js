c6921260c8cbd79426d7fa979e0a6b0024b4726a
sceext2 <sceext@foxmail.com>
2020-03-12 16:43:48 +0800

    improve no_c2a0.js: replace with ASCII space, NOT remove

--- src/no_c2a0.js
+++ src/no_c2a0.js
@@ -1,60 +1,52 @@
 // no_c2a0.js
 // clean 0xc2a0 (utf-8 encode, U+00A0, Unicode NO BREAKING SPACE in text files
-// usage:  node no_c2a0.js 1.txt 2.txt
+// usage: node no_c2a0.js 1.txt 2.txt
 
 const fs = require("fs");
 
 const CODE_C2A0 = 160;
 
+
 function clean(f) {
+  console.log("CLEAN " + f);
 
-console.log("CLEAN  " + f);
+  const b = fs.readFileSync(f);
+  const t = b.toString("utf-8");
 
-const b = fs.readFileSync(f);
-const t = b.toString("utf-8");
+  let o = "";
+  let count = 0;
 
-let o = "";
+  for (let i = 0; i < t.length; i += 1) {
+    const c = t.charCodeAt(i);
+    if (c != CODE_C2A0) {
+      o += t[i];
+    } else {
+      // replace c2a0 with ASCII space, NOT remove
+      o += " ";
+      count += 1;
+    }
 
-for (let i = 0; i < t.length; i += 1) {
+    //if (c < 128) {
+    //  o += t[i];
+    //} else {
+    //  console.log("pos " + i + ", code " + c);
+    //}
+  }
 
-const c = t.charCodeAt(i);
-
-if (c != CODE_C2A0) {
-
-o += t[i];
-
+  fs.writeFileSync(f, o, "utf-8");
+  if (count > 0) {
+    console.log("  - " + count);
+  }
 }
 
-continue;
-// FIXME
-if (c < 128) {
+function no_c2a0() {
+  const f = process.argv.slice(2);
 
-o += t[i];
-
-} else {
-
-console.log("pos " + i + ", code " + c);
-
+  for (let i of f) {
+    clean(i);
+  }
 }
 
-}
+no_c2a0();
 
-fs.writeFileSync(f, o, "utf-8");
-
-}
-
-function main() {
-
-const f = process.argv.slice(2);
-
-for (let i of f) {
-
-clean(i);
-
-}
-
-}
-
-main();
-
-// 20200312 0349
+// 20200312 1624
--- src/trim_line.js
+++ src/trim_line.js
@@ -6,24 +6,24 @@ const fs = require("fs");
 
 
 function trim_one(f) {
-  const t = fs.readFileSync(f, "utf-8");
-  const l = t.split("\n");
-  const o = [];
-  for (let i of l) {
-    if (i.trim().length < 1) {
-      o.push("");
-    } else {
-      o.push(i);
-    }
-  }
-  fs.writeFileSync(f, o.join("\n"), "utf-8");
+  const t = fs.readFileSync(f, "utf-8");
+  const l = t.split("\n");
+  const o = [];
+  for (let i of l) {
+    if (i.trim().length < 1) {
+      o.push("");
+    } else {
+      o.push(i);
+    }
+  }
+  fs.writeFileSync(f, o.join("\n"), "utf-8");
 }
 
 function trim_main() {
-  for (let i of process.argv.slice(2)) {
-    console.log("TRIM  " + i);
-    trim_one(i);
-  }
+  for (let i of process.argv.slice(2)) {
+    console.log("TRIM  " + i);
+    trim_one(i);
+  }
 }
 
 trim_main();
